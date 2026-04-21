ac919be8237370070e58ec2615eff2bb0d1930a5
anoop0155 <anoop.sharma@planify.in>
2026-04-22 00:37:21 +0530

    error fix

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -25,6 +25,10 @@ CSRF_TRUSTED_ORIGINS = [
     "https://fixngo-theta.vercel.app",
 ]
 
-INSTALLED_APPS += ["debug_toolbar"]  
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  
-INTERNAL_IPS = ["127.0.0.1"]
+try:
+    import debug_toolbar
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+    INTERNAL_IPS = ["127.0.0.1"]
+except ImportError:
+    pass
